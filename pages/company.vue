0d73ed1ce4b3ede3ec32dbe15492fcfcc64551cf
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2023-08-08 15:02:29 +0500

    css

--- pages/company.vue
+++ pages/company.vue
@@ -216,6 +216,45 @@
               </div>
             </div>
           </div>
+          <div class="pagination">
+              <div class="pagination_icon">
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M15 6L9 12L15 18"
+                    stroke="black"
+                    stroke-width="2.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+              <div class="pagination_count" v-for="p in 5" :key="p">
+                {{ p }}
+              </div>
+              <div class="pagination_icon">
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M9 18L15 12L9 6"
+                    stroke="black"
+                    stroke-width="2.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+            </div>
         </div>
       </div>
     </div>
