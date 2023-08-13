b522deae595a89f14304186e8951ca76d0c0ba23
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2023-08-13 16:10:09 +0500

    submit

--- pages/add.vue
+++ pages/add.vue
@@ -5,89 +5,44 @@
     <div class="add_page">
       <div class="container">
         <div class="add_info">
-          <div class="product">
-            <div class="product_name">Mahsulot nomi</div>
+          <div class="product_add">
+            <div class="product_add_father">
+              <div class="selected" :class="{ active: selectModal }">
+                <div
+                  class="selected_option"
+                  @click="selectModal = !selectModal"
+                >
+                  <div class="selected_option_title">kategoriyalar</div>
+                  <div class="selected_option_icon">
+                    <svg
+                      width="30"
+                      height="30"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 448 512"
+                    >
+                      <path
+                        d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <div class="selected_content">
+                  <div
+                    class="selected_content_title"
+                    @click="categoryFather()"
+                    v-for="p in 10"
+                    :key="p"
+                  >
+                    kiyilar
+                  </div>
+                </div>
+              </div>
 
-            <input
-              class="product_input"
-              type="text"
-              placeholder="mahsulot nomini kiriting"
-            />
-            <div class="product_category">
-              <div class="product_category_option">Mahsulot toifasi</div>
-              <div class="product_category_modal">
-                <div class="close_btn">
-                  <div class="btn"></div>
-                </div>
-                <div class="product_category_modal_title">Toifani tanlang</div>
-                <div class="categorys">
-                  <div
-                    id="cat-1"
-                    class="categorys_category"
-                    @click="activeFun($event)"
-                  >
-                    <div class="categorys_category_title">
-                      <div class="categorys_category_title_text">toifa</div>
-                      <div class="categorys_category_title_icon">
-                        <svg
-                          width="30"
-                          height="30"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 448 512"
-                        >
-                          <path
-                            d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"
-                          />
-                        </svg>
-                      </div>
-                    </div>
-                    <div class="categorys_category_content">
-                      <div class="categorys_category_content_title">
-                        toifa 1
-                      </div>
-                      <div class="categorys_category_content_title">
-                        toifa 2
-                      </div>
-                      <div class="categorys_category_content_title">
-                        toifa 3
-                      </div>
-                    </div>
-                  </div>
-                  <div
-                    id="cat-2"
-                    class="categorys_category"
-                    @click="activeFun($event)"
-                  >
-                    <div class="categorys_category_title">
-                      <div class="categorys_category_title_text">
-                        categoriya
-                      </div>
-                      <div class="categorys_category_title_icon">
-                        <svg
-                          width="30"
-                          height="30"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 448 512"
-                        >
-                          <path
-                            d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"
-                          />
-                        </svg>
-                      </div>
-                    </div>
-                    <div class="categorys_category_content">
-                      <div class="categorys_category_content_title">
-                        categoriya 1
-                      </div>
-                      <div class="categorys_category_content_title">
-                        categoriya 2
-                      </div>
-                      <div class="categorys_category_content_title">
-                        categoriya 3
-                      </div>
-                    </div>
-                  </div>
-                </div>
+            </div>
+            <div class="product_add_child">
+              <div class="info" >
+                <label class="info_label" for="child-1">children</label>
+                <input class="info_input" type="text" id="child-1">
               </div>
             </div>
           </div>
@@ -98,18 +53,9 @@
 </template>
   
   <script setup>
-
-function activeFun(e) {
-  document.querySelectorAll(".categorys .categorys_category").forEach((el) => {
-    // if (e.target.parentElement.id == el.id) {
-    //   e.target.parentElement.classList.toggle("active");
-    //   console.log("hello");
-    // } else {
-    //   e.target.parentElement.classList.remove("active");
-    //   console.log("bug");
-    // }
-  });
-  console.log(e.target);
+const selectModal = ref(false);
+function categoryFather() {
+  selectModal.value = false;
 }
 </script>
 
