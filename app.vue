f20d6f18080ab4205c13501aeeca7bdaed249b1d
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2023-08-15 09:38:14 +0500

    prodeucd edit

--- app.vue
+++ app.vue
@@ -9,10 +9,4 @@ if(!localStorage.getItem("userToken")){
   router.push("/login")
 };
 
-// setTimeout(() => {
-//   localStorage.removeItem("userToken");
-// }, "21000");
-// setInterval(function(){
-//                 alert('this is executed for each second');
-//               }, 5000);
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config  
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css:['maz-ui/css/main.css',"~/assets/scss/main.scss"],
+  css: ['maz-ui/css/main.css', "~/assets/scss/main.scss",],
   modules: [
     '@nuxtjs/i18n',
   ],
   i18n: {
     lazy: true,
     langDir: "locales",
-    defaultLocale:"uz",
-    locales:[
+    defaultLocale: "uz",
+    locales: [
       {
-        code:"uz",
-        file:"uz.json"
+        code: "uz",
+        file: "uz.json"
       },
       {
-        code:"ru",
-        file:"ru.json"
+        code: "ru",
+        file: "ru.json"
       },
     ]
   },
-  runtimeConfig:{
-    public:{
-      baseUrl:"https://lionprintspringboot-production.up.railway.app/api/v1"
+  runtimeConfig: {
+    public: {
+      baseUrl: "https://lionprintspringboot-production.up.railway.app/api/v1"
     }
   },
   build: {
     transpile: ['maz-ui'],
   },
-  ssr:false
+  ssr: false
 })
