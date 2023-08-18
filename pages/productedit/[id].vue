a11543e4f067c1bf964963322a4842e8a5b41a33
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2023-08-18 11:06:17 +0500

    product edit

--- pages/productedit/[id].vue
+++ pages/productedit/[id].vue
@@ -1,12 +1,9 @@
 <template>
   <div>
-    <!-- <div class="bg"></div> -->
-
     <div class="add_page">
       <div class="container">
         <div class="add_info">
           <div class="product_add">
-            <!-- <pre>{{ categoryChild }}</pre> -->
             <div class="product_add_father">
               <div class="selected" :class="{ active: selectModal }">
                 <div
@@ -166,7 +163,7 @@
                   />
                 </div>
               </div>
-              <div class="submit_icons" >
+              <div class="submit_icons">
                 <div class="submit_icons_btn exit">Orqaga</div>
                 <div class="submit_icons_btn send" @click="productEditApi">
                   yuborish
@@ -181,9 +178,13 @@
 </template>
     
     <script setup>
+import { useToast } from "vue-toastification";
+const toast = useToast();
+import { useStore } from "~/store/store";
+const store = useStore();
 const baseUrl = useRuntimeConfig().public.baseUrl;
 const route = useRoute();
-const router = useRouter()
+const router = useRouter();
 const { locale } = useI18n();
 const { id } = route.params;
 const selectModal = ref(false);
@@ -192,16 +193,45 @@ const currensyTitle = ref("valyuta turi");
 const currensy = ref(null);
 const currencyId = ref(null);
 async function currensyApi() {
-  const data = await $fetch(baseUrl + "/currency-type/all", {
-    method: "GET",
-    params: {
-      companyId: localStorage.getItem("userId"),
-    },
-    headers: {
-      Authorization: "Bearer " + localStorage.getItem("userToken"),
-    },
-  });
-  currensy.value = data;
+  try {
+    store.loader = true;
+    const data = await $fetch(baseUrl + "/currency-type/all", {
+      method: "GET",
+      params: {
+        companyId: localStorage.getItem("userId"),
+      },
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("userToken"),
+        "Accept-Language": locale.value,
+      },
+    });
+
+    currensy.value = data;
+    if (data) {
+      store.loader = false;
+    }
+  } catch (error) {
+    store.loader = false;
+    toast.error(
+      error?.response?._data?.message ||
+        error?.response?._data?.error ||
+        "Error",
+      {
+        position: "top-right",
+        timeout: 2000,
+        closeOnClick: true,
+        pauseOnFocusLoss: true,
+        pauseOnHover: true,
+        draggable: true,
+        draggablePercent: 0.6,
+        showCloseButtonOnHover: false,
+        hideProgressBar: true,
+        closeButton: "button",
+        icon: true,
+        rtl: false,
+      }
+    );
+  }
 }
 function currensyChildApi(e) {
   currensyModal.value = false;
@@ -211,17 +241,45 @@ function currensyChildApi(e) {
 currensyApi();
 const categoryFatherInfo = ref(null);
 async function categoryFatherApi() {
-  const data = await $fetch(baseUrl + "/category", {
-    method: "GET",
-    headers: {
-      Authorization: "Bearer " + localStorage.getItem("userToken"),
-    },
-    params: {
-      page: 0,
-      size: 10,
-    },
-  });
-  categoryFatherInfo.value = data;
+  try {
+    store.loader = true;
+    const data = await $fetch(baseUrl + "/category", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("userToken"),
+        "Accept-Language": locale.value,
+      },
+      params: {
+        page: 0,
+        size: 10,
+      },
+    });
+    categoryFatherInfo.value = data;
+    if (data) {
+      store.loader = false;
+    }
+  } catch (error) {
+    store.loader = false;
+    toast.error(
+      error?.response?._data?.message ||
+        error?.response?._data?.error ||
+        "Error",
+      {
+        position: "top-right",
+        timeout: 2000,
+        closeOnClick: true,
+        pauseOnFocusLoss: true,
+        pauseOnHover: true,
+        draggable: true,
+        draggablePercent: 0.6,
+        showCloseButtonOnHover: false,
+        hideProgressBar: true,
+        closeButton: "button",
+        icon: true,
+        rtl: false,
+      }
+    );
+  }
 }
 const input = ref(null);
 
@@ -230,57 +288,139 @@ const inputFatherTitle = ref("mahsulotlar katalogi");
 const categoryChild = ref(null);
 const categoryFatherId = ref(null);
 async function categoryChildApi(e) {
-  selectModal.value = false;
-  categoryFatherId.value = e.id;
-  inputFatherTitle.value = e.name;
-  const data = await $fetch(baseUrl + "/category-detail/all", {
-    method: "GET",
-    headers: {
-      Authorization: "Bearer " + localStorage.getItem("userToken"),
-      "Accept-Language": locale.value,
-    },
-    params: {
-      categoryId: e.id,
-    },
-  });
-  categoryChild.value = data;
+  try {
+    store.loader = true;
+    selectModal.value = false;
+    categoryFatherId.value = e.id;
+    inputFatherTitle.value = e.name;
+    const data = await $fetch(baseUrl + "/category-detail/all", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("userToken"),
+        "Accept-Language": locale.value,
+      },
+      params: {
+        categoryId: e.id,
+      },
+    });
+    categoryChild.value = data;
+    if (data) {
+      store.loader = false;
+    }
+  } catch (error) {
+    store.loader = false;
+    toast.error(
+      error?.response?._data?.message ||
+        error?.response?._data?.error ||
+        "Error",
+      {
+        position: "top-right",
+        timeout: 2000,
+        closeOnClick: true,
+        pauseOnFocusLoss: true,
+        pauseOnHover: true,
+        draggable: true,
+        draggablePercent: 0.6,
+        showCloseButtonOnHover: false,
+        hideProgressBar: true,
+        closeButton: "button",
+        icon: true,
+        rtl: false,
+      }
+    );
+  }
 }
+
 const produyctList = ref(null);
 async function productListApi() {
-  const data = await $fetch(baseUrl + `/product/${id}`, {
-    method: "GET",
-    headers: {
-      Authorization: "Bearer " + localStorage.getItem("userToken"),
-      "Accept-Language": locale.value,
-    },
-  });
-  produyctList.value = data;
-  inputFatherTitle.value = data?.categoryName;
-  price.value = data?.price;
-  productDate.value.value = data?.factoryDate;
-  min.value = data?.minAmount;
-  max.value = data?.maxAmount;
-  textarea.value = data?.description;
-  currensyTitle.value = data?.currencyName + " " + data?.currencySymbol;
-  ndsInput.value = data?.hasNds;
-  deliveryInput.value = data?.hasDelivery
-  categoryFatherId.value = data?.categoryId;
-  currencyId.value = data?.currencyId
+  try {
+    store.loader = true;
+    const data = await $fetch(baseUrl + `/product/${id}`, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("userToken"),
+        "Accept-Language": locale.value,
+      },
+    });
+    produyctList.value = data;
+    inputFatherTitle.value = data?.categoryName;
+    price.value = data?.price;
+    productDate.value.value = data?.factoryDate;
+    min.value = data?.minAmount;
+    max.value = data?.maxAmount;
+    textarea.value = data?.description;
+    currensyTitle.value = data?.currencyName + " " + data?.currencySymbol;
+    ndsInput.value = data?.hasNds;
+    deliveryInput.value = data?.hasDelivery;
+    categoryFatherId.value = data?.categoryId;
+    currencyId.value = data?.currencyId;
+    if (data) {
+      store.loader = false;
+    }
+  } catch (error) {
+    store.loader = false;
+    toast.error(
+      error?.response?._data?.message ||
+        error?.response?._data?.error ||
+        "Error",
+      {
+        position: "top-right",
+        timeout: 2000,
+        closeOnClick: true,
+        pauseOnFocusLoss: true,
+        pauseOnHover: true,
+        draggable: true,
+        draggablePercent: 0.6,
+        showCloseButtonOnHover: false,
+        hideProgressBar: true,
+        closeButton: "button",
+        icon: true,
+        rtl: false,
+      }
+    );
+  }
 }
 productListApi();
-async function getCategoryApi(){
-  const data = await $fetch(baseUrl + "/category-detail/all", {
-    method: "GET",
-    headers: {
-      Authorization: "Bearer " + localStorage.getItem("userToken"),
-      "Accept-Language": locale.value,
-    },
-    params: {
-      categoryId: categoryFatherId.value,
-    },
-  });
-  categoryChild.value = data;
-};
+async function getCategoryApi() {
+  try {
+    store.loader = true;
+    const data = await $fetch(baseUrl + "/category-detail/all", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("userToken"),
+        "Accept-Language": locale.value,
+      },
+      params: {
+        categoryId: categoryFatherId.value,
+      },
+    });
+    categoryChild.value = data;
+    if (data) {
+      store.loader = false;
+    }
+  } catch (error) {
+    store.loader = false;
+    toast.error(
+      error?.response?._data?.message ||
+        error?.response?._data?.error ||
+        "Error",
+      {
+        position: "top-right",
+        timeout: 2000,
+        closeOnClick: true,
+        pauseOnFocusLoss: true,
+        pauseOnHover: true,
+        draggable: true,
+        draggablePercent: 0.6,
+        showCloseButtonOnHover: false,
+        hideProgressBar: true,
+        closeButton: "button",
+        icon: true,
+        rtl: false,
+      }
+    );
+  }
+}
 getCategoryApi();
 const ndsInput = ref(false);
 const deliveryInput = ref(false);
@@ -290,38 +430,74 @@ const max = ref(null);
 const price = ref(null);
 const textarea = ref();
 async function productEditApi() {
-  const arr = [];
-  input.value.forEach((elem, i) => {
-    arr.push({
-      categoryDetailId: categoryChild.value[i].id,
-      value: elem.value,
+  try {
+    store.loader = true;
+    const arr = [];
+    input.value.forEach((elem, i) => {
+      arr.push({
+        categoryDetailId: categoryChild.value[i].id,
+        value: elem.value,
+      });
     });
-  });
-  const data = await $fetch(baseUrl + "/product", {
-    method: "PUT",
-    headers: {
-      Authorization: "Bearer " + localStorage.getItem("userToken"),
-    },
-    body: JSON.stringify({
-      id: id,
-      description: textarea.value,
-      price: price.value,
-      hasDelivery: deliveryInput.value,
-      hasNds: ndsInput.value,
-      currencyId: currencyId.value,
-      categoryId: categoryFatherId.value,
-      maxAmount: max.value,
-      minAmount: min.value,
-      factoryDate:productDate.value.value,
-      supplierId: localStorage.getItem("userSupplierId"),
-      details: arr,
-    }),
-  });
-  if(data?.message == "ok"){
-    console.log("hello");
-    router.push("/")
-  }else{
-    console.log("bug");
+    const data = await $fetch(baseUrl + "/product", {
+      method: "PUT",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("userToken"),
+        "Accept-Language": locale.value,
+      },
+      body: JSON.stringify({
+        id: id,
+        description: textarea.value,
+        price: price.value,
+        hasDelivery: deliveryInput.value,
+        hasNds: ndsInput.value,
+        currencyId: currencyId.value,
+        categoryId: categoryFatherId.value,
+        maxAmount: max.value,
+        minAmount: min.value,
+        factoryDate: productDate.value.value,
+        supplierId: localStorage.getItem("userSupplierId"),
+        details: arr,
+      }),
+    });
+    if (data?.message == "ok") {
+      router.push("/");
+      toast.success(data?.message || "Success", {
+        position: "top-right",
+        timeout: 2000,
+        closeOnClick: true,
+        pauseOnFocusLoss: true,
+        pauseOnHover: true,
+        draggable: true,
+        draggablePercent: 0.6,
+        showCloseButtonOnHover: false,
+        hideProgressBar: true,
+        closeButton: "button",
+        icon: true,
+        rtl: false,
+      });
+    }
+  } catch (error) {
+    store.loader = false;
+    toast.error(
+      error?.response?._data?.message ||
+        error?.response?._data?.error ||
+        "Error",
+      {
+        position: "top-right",
+        timeout: 2000,
+        closeOnClick: true,
+        pauseOnFocusLoss: true,
+        pauseOnHover: true,
+        draggable: true,
+        draggablePercent: 0.6,
+        showCloseButtonOnHover: false,
+        hideProgressBar: true,
+        closeButton: "button",
+        icon: true,
+        rtl: false,
+      }
+    );
   }
 }
 </script>
