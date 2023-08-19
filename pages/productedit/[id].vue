12e3f9874939ecb8a6ccbb0c0747eb563b29f3c1
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2023-08-19 15:22:15 +0500

    push product

--- pages/productedit/[id].vue
+++ pages/productedit/[id].vue
@@ -37,6 +37,38 @@
                   </div>
                 </div>
               </div>
+              <div class="selected" :class="{ active: currensyModal }">
+                <div
+                  class="selected_option"
+                  @click="currensyModal = !currensyModal"
+                >
+                  <div class="selected_option_title">
+                    {{ currensyTitle }}
+                  </div>
+                  <div class="selected_option_icon">
+                    <svg
+                      width="30"
+                      height="30"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 448 512"
+                    >
+                      <path
+                        d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <div class="selected_content">
+                  <div
+                    class="selected_content_title"
+                    v-for="c in currensy"
+                    :key="c?.id"
+                    @click="currensyChildApi(c)"
+                  >
+                    {{ c?.nameUz }}
+                  </div>
+                </div>
+              </div>
               <div class="info">
                 <input
                   class="info_input"
@@ -110,38 +142,7 @@
                   v-model="max"
                 />
               </div>
-              <div class="selected" :class="{ active: currensyModal }">
-                <div
-                  class="selected_option"
-                  @click="currensyModal = !currensyModal"
-                >
-                  <div class="selected_option_title">
-                    {{ currensyTitle }}
-                  </div>
-                  <div class="selected_option_icon">
-                    <svg
-                      width="30"
-                      height="30"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 448 512"
-                    >
-                      <path
-                        d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"
-                      />
-                    </svg>
-                  </div>
-                </div>
-                <div class="selected_content">
-                  <div
-                    class="selected_content_title"
-                    v-for="c in currensy"
-                    :key="c?.id"
-                    @click="currensyChildApi(c)"
-                  >
-                    {{ c?.nameUz }}
-                  </div>
-                </div>
-              </div>
+          
               <div class="price">
                 <label for="amount-2" class="price_label">mahsulot narxi</label>
                 <input
